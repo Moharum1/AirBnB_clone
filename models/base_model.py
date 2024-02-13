7c7baaea85c5d8a543b6cc9e0875af25ba02bf84
Moharum1 <moharum4@gamil.com>
2024-02-13 15:11:10 +0200

    done

--- models/base_model.py
+++ models/base_model.py
@@ -23,8 +23,8 @@ class BaseModel():
         """
         if not kwargs:
             self.id = str(uuid.uuid4())
-            self.created_at = datetime.utcnow()
-            self.updated_at = datetime.utcnow()
+            self.created_at = datetime.now()
+            self.updated_at = self.created_at
             models.storage.new(self)
 
         else:
